87a8d319211c7bb889908470bcd3771abaca4bd4
Sebastian Lienau <16371421+sebastianlienau@users.noreply.github.com>
2019-07-07 19:12:16 +0200

    add scripts

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -2,20 +2,20 @@
 
 docker build \
 -t node-red \
--f Dockerfile .
+-f ../Dockerfile ..
 
 docker build \
 -t node-red:slim \
 --build-arg BASE_IMAGE_SUFFIX="-slim" \
--f Dockerfile .
+-f ../Dockerfile ..
 
 docker build \
 -t node-red:arm \
 --build-arg BASE_NODE_IMAGE="arm32v7/node" \
--f Dockerfile .
+-f ../Dockerfile ..
 
 docker build \
 -t node-red:arm-slim \
 --build-arg BASE_NODE_IMAGE="arm32v7/node" \
 --build-arg BASE_IMAGE_SUFFIX="-slim" \
--f Dockerfile .
+-f ../Dockerfile ..
