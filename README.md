af75c94c553c03c01a054a8fbc7c0515338d8f0b
Sebastian Lienau <16371421+sebastianlienau@users.noreply.github.com>
2019-08-09 12:25:36 +0200

    Add docker client to NodeRedFogNode

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # Fog orchestrator
 
 The Fog orchestrator collects data about every fog nodes system load and about the overall network quality. Based on that it calculates and deploys optimal deployments to ensure best QoS.
+
+## Setup
+
+### Expose Docker API on fog nodes
+
+Expose the Docker API on port `52376`
+
+#### Raspberry Pi / Linux
+
+Create file `/etc/systemd/system/docker.service.d/remote-api.conf` with content (replacing `127.0.0.1` with the address to listen):
+
+```bash
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd -H tcp://127.0.0.1:2376 -H unix:///var/run/docker.sock
+```
+
+Restart Docker:
+
+```bash
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+```
+
+#### macOS
+
+Using the tool `socat` (by running it inside its own docker container and passing the docker socket)
+
+```bash
+docker run -d --name socat-docker --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -p 52376:2375 bobrik/socat TCP4-LISTEN:2375,fork,reuseaddr UNIX-CONNECT:/var/run/docker.sock
+```
--- src/main/java/de/tuberlin/aot/thesis/slienau/orchestrator/NodeRedFogNode.java
+++ src/main/java/de/tuberlin/aot/thesis/slienau/orchestrator/NodeRedFogNode.java
@@ -1,5 +1,8 @@
 package de.tuberlin.aot.thesis.slienau.orchestrator;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
 import de.tuberlin.aot.thesis.slienau.scheduler.infrastructure.FogNode;
 
 import java.util.List;
@@ -7,10 +10,15 @@ import java.util.List;
 public class NodeRedFogNode extends FogNode {
 
     private final NodeRedController nodeRedController;
+    private final DockerClient dockerClient;
 
     public NodeRedFogNode(String id, String address, int ramTotal, int storageTotal, int cpuCores, int cpuInstructionsPerSecond, List<String> connectedHardware) {
         super(id, ramTotal, storageTotal, cpuCores, cpuInstructionsPerSecond, connectedHardware);
         nodeRedController = new NodeRedController(id, address);
+        DefaultDockerClientConfig config
+                = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerHost(String.format("tcp://%s:52376", address)).build();
+        dockerClient = DockerClientBuilder.getInstance(config).build();
     }
 
     public NodeRedController getNodeRedController() {
@@ -19,6 +27,10 @@ public class NodeRedFogNode extends FogNode {
 
     public String getAddress() {
         return nodeRedController.getNodeRedAddress();
+    }
+
+    public DockerClient getDockerClient() {
+        return dockerClient;
     }
 
     @Override
