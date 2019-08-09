a015b350193376e5b7281e8791eb36c37d8296cf
Sebastian Lienau <16371421+sebastianlienau@users.noreply.github.com>
2019-08-09 12:35:20 +0200

    Cleanup code in NodeRedOrchestrator

--- src/main/java/de/tuberlin/aot/thesis/slienau/orchestrator/NodeRedOrchestrator.java
+++ src/main/java/de/tuberlin/aot/thesis/slienau/orchestrator/NodeRedOrchestrator.java
@@ -2,6 +2,7 @@ package de.tuberlin.aot.thesis.slienau.orchestrator;
 
 import de.tuberlin.aot.thesis.slienau.scheduler.application.AppSoftwareModule;
 import de.tuberlin.aot.thesis.slienau.scheduler.application.Application;
+import de.tuberlin.aot.thesis.slienau.scheduler.infrastructure.FogNode;
 import de.tuberlin.aot.thesis.slienau.scheduler.infrastructure.Infrastructure;
 import de.tuberlin.aot.thesis.slienau.scheduler.interfaces.Scheduler;
 import de.tuberlin.aot.thesis.slienau.scheduler.strategy.AppDeployment;
@@ -9,24 +10,27 @@ import de.tuberlin.aot.thesis.slienau.scheduler.strategy.SchedulerStrategy;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class NodeRedOrchestrator {
 
-    private Map<String, NodeRedFogNode> nodeRedInstances = new HashMap<>();
+    private static final NodeRedFlowDatabase flowDatabase = NodeRedFlowDatabase.getInstance();
+    private final Infrastructure infrastructure;
+
+    public NodeRedOrchestrator() {
+        infrastructure = new Infrastructure();
+    }
 
     public static void main(String[] args) throws IOException {
         System.out.println("[NodeRedOrchestrator] Starting...");
 
-        Application a = getSensorNetworkApplication();
-
-        Infrastructure i = new Infrastructure();
+        NodeRedOrchestrator orchestrator = new NodeRedOrchestrator();
+        Infrastructure i = orchestrator.infrastructure;
+        Application a = createSensorNetworkApplication();
 
         NodeRedFogNode mbp = new NodeRedFogNode("mbp", "dsl-mbp", 1024 * 16, 256, 8, 16000, null);
-        NodeRedFogNode raspi1 = new NodeRedFogNode("raspi-01", "raspi-01", 1024 * 1, 32, 4, 1000, null);
+        NodeRedFogNode raspi1 = new NodeRedFogNode("raspi-01", "raspi-01", 1024 * 1, 32, 4, 1000, Arrays.asList("CAMERA"));
         NodeRedFogNode raspi2 = new NodeRedFogNode("raspi-02", "raspi-02", 1024 * 4, 16, 4, 3000, null);
         i.addFogNode(mbp);
         i.addFogNode(raspi1);
@@ -40,7 +44,11 @@ public class NodeRedOrchestrator {
         AppDeployment d = s.getFastestDeployment();
         System.out.println(d.createDetailsString());
 
-        NodeRedFlowDatabase flowDatabase = NodeRedFlowDatabase.getInstance();
+        for (FogNode fn : i.getFogNodes()) {
+            NodeRedFogNode fogNode = (NodeRedFogNode) fn;
+            fogNode.getNodeRedController().deleteAllFlows();
+        }
+
 
         d.getModuleToNodeMap().entrySet().stream().forEach(entry -> {
             AppSoftwareModule module = entry.getKey();
@@ -63,10 +71,11 @@ public class NodeRedOrchestrator {
 
     }
 
-    private static Application getSensorNetworkApplication() {
+    private static Application createSensorNetworkApplication() {
         Application a = new Application("sensornetwork");
 
-        a.addSoftwareModule("data-reader", "RAW_SENSOR_DATA", "SENSOR_DATA", 50, 0.5, 1000, null);
+        a.addHardwareModule("CAMERA", "RAW_SENSOR_DATA");
+        a.addSoftwareModule("data-reader", "RAW_SENSOR_DATA", "SENSOR_DATA", 50, 0.5, 500, Arrays.asList("CAMERA"));
         a.addSoftwareModule("data-processor", "SENSOR_DATA", "SENSOR_DATA_PROCESSED", 100, 0.5, 5000, null);
         a.addSoftwareModule("data-viewer", "SENSOR_DATA_PROCESSED", null, 100, 0.5, 500, null);
 
@@ -74,7 +83,7 @@ public class NodeRedOrchestrator {
         a.addMessage("SENSOR_DATA", 10);
         a.addMessage("SENSOR_DATA_PROCESSED", 10);
 
-        a.addLoop("sensorNetworkLoop1", 500, Arrays.asList("data-reader", "data-processor", "data-viewer"));
+        a.addLoop("sensorNetworkLoop1", 1500, Arrays.asList("data-reader", "data-processor", "data-viewer"));
         return a;
     }
 }
