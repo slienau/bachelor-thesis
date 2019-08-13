542ff84543fbee147ee08dd3746c314a9e372e4a
Sebastian Lienau <16371421+sebastianlienau@users.noreply.github.com>
2019-08-13 20:04:54 +0200

    Unblock heartbeatQueue

--- src/main/java/de/tuberlin/aot/thesis/slienau/orchestrator/monitor/HeartbeatProcessor.java
+++ src/main/java/de/tuberlin/aot/thesis/slienau/orchestrator/monitor/HeartbeatProcessor.java
@@ -22,6 +22,7 @@ public class HeartbeatProcessor implements Runnable {
     public void run() {
         System.out.println("[HeartbeatProcessor] Started...");
         while (true) {
+            Heartbeat hb = null;
             synchronized (heartbeatQueue) {
                 while (heartbeatQueue.isEmpty()) {
                     try {
@@ -30,46 +31,45 @@ public class HeartbeatProcessor implements Runnable {
                         e.printStackTrace();
                     }
                 }
-
-                Heartbeat hb = heartbeatQueue.remove();
-//                System.out.println(String.format("[HeartbeatProcessor] Received %s", hb));
-                if (infrastructure.getFogNode(hb.getDeviceName()) == null) {
-                    // new node --> add
-                    NodeRedFogNode newNode = new NodeRedFogNode(
-                            hb.getDeviceName(),
-                            hb.getDeviceName(),
-                            hb.getTotalMem(),
-                            NumberUtils.getRandom(16, 100),
-                            hb.getCpuCount(),
-                            NumberUtils.getRandom(3000, 10000),
-                            null
-                    );
-
-                    try {
-                        // delete all flows on new node (in case they have "old" flows deployed which could disturb the current deployment strategy)
-                        newNode.getNodeRedController().deleteAllFlows();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    infrastructure.addFogNode(newNode);
-
-                    // add network uplinks to all other nodes
-                    List<String> destinationNodeIds = infrastructure.getFogNodes().stream()
-                            .filter(node -> !node.getId().equals(newNode.getId()))
-                            .map(node -> node.getId())
-                            .collect(Collectors.toList());
-                    for (String destinationNodeId : destinationNodeIds) {
-                        infrastructure.addNetworkLink(
-                                newNode.getId(),
-                                destinationNodeId,
-                                NumberUtils.getRandom(2, 200),
-                                NumberUtils.getRandom(10, 250),
-                                NumberUtils.getRandom(10, 250)
-                        );
-                    }
-                }
+                hb = heartbeatQueue.remove();
                 heartbeatQueue.notifyAll();
+            }
+            //                System.out.println(String.format("[HeartbeatProcessor] Received %s", hb));
+            if (infrastructure.getFogNode(hb.getDeviceName()) == null) {
+                // new node --> add
+                NodeRedFogNode newNode = new NodeRedFogNode(
+                        hb.getDeviceName(),
+                        hb.getDeviceName(),
+                        hb.getTotalMem(),
+                        NumberUtils.getRandom(16, 100),
+                        hb.getCpuCount(),
+                        NumberUtils.getRandom(3000, 10000),
+                        null
+                );
+
+                try {
+                    // delete all flows on new node (in case they have "old" flows deployed which could disturb the current deployment strategy)
+                    newNode.getNodeRedController().deleteAllFlows();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                infrastructure.addFogNode(newNode);
+
+                // add network uplinks to all other nodes
+                List<String> destinationNodeIds = infrastructure.getFogNodes().stream()
+                        .filter(node -> !node.getId().equals(newNode.getId()))
+                        .map(node -> node.getId())
+                        .collect(Collectors.toList());
+                for (String destinationNodeId : destinationNodeIds) {
+                    infrastructure.addNetworkLink(
+                            newNode.getId(),
+                            destinationNodeId,
+                            NumberUtils.getRandom(2, 200),
+                            NumberUtils.getRandom(10, 250),
+                            NumberUtils.getRandom(10, 250)
+                    );
+                }
             }
         }
     }
