db29f650eb92a617ae87c37751f27df3133ed60e
Sebastian Lienau <lienau@campus.tu-berlin.de>
2019-09-16 14:27:07 +0200

    Change MQTT broker to broker on azure

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,7 +2,7 @@ export DOCKER_CONTAINER_NAME=nodered
 export DOCKER_IMAGE=node-red
 export DOCKER_VOLUME_NAME=nodered-data
 export DOCKER_REGISTRY=dsl-mbp
-export MQTT_SERVER=192.168.7.13
+export MQTT_SERVER=dslbabroker.westeurope.azurecontainer.io
 export CONNECTED_HARDWARE="[]"
 # export CONNECTED_HARDWARE="[\"CAMERA\",\"TEMPERATURE\"]"
 
