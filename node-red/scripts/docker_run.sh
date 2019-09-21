69abcb6d0a193d4c89cddf9cbc716f171ecbbdf0
Sebastian Lienau <lienau@campus.tu-berlin.de>
2019-09-21 21:23:10 +0100

    Disable CPU limit on arm architecture because it is not supported

--- node-red/scripts/docker_run.sh
+++ node-red/scripts/docker_run.sh
@@ -10,15 +10,30 @@ if [ -e "$VIDEO_DEVICE" ]; then
     DOCKER_DEVICE_PARAM="--device=${VIDEO_DEVICE}:${VIDEO_DEVICE} "
 fi
 
+# set CPU limit for non-arm architecture (not supported by arm)
+if [[ "$(uname -m)" == "arm"* ]]; then
 docker run -it -d $DOCKER_DEVICE_PARAM\
---restart=unless-stopped \
---name nodered \
---hostname=$HOSTNAME \
--v nodered-data:/data \
--e MQTT_SERVER=$MQTT_SERVER \
--e CONNECTED_HARDWARE=$CONNECTED_HARDWARE \
--e PUBLIC_FQDN=$PUBLIC_FQDN \
--e PUBLIC_PORT=$PUBLIC_PORT \
--p $PUBLIC_PORT:1880 \
---cpus=$CPU_LIMIT \
-node-red
+    --restart=unless-stopped \
+    --name nodered \
+    --hostname=$HOSTNAME \
+    -v nodered-data:/data \
+    -e MQTT_SERVER=$MQTT_SERVER \
+    -e CONNECTED_HARDWARE=$CONNECTED_HARDWARE \
+    -e PUBLIC_FQDN=$PUBLIC_FQDN \
+    -e PUBLIC_PORT=$PUBLIC_PORT \
+    -p $PUBLIC_PORT:1880 \
+    node-red
+else
+    docker run -it -d $DOCKER_DEVICE_PARAM\
+    --restart=unless-stopped \
+    --name nodered \
+    --hostname=$HOSTNAME \
+    -v nodered-data:/data \
+    -e MQTT_SERVER=$MQTT_SERVER \
+    -e CONNECTED_HARDWARE=$CONNECTED_HARDWARE \
+    -e PUBLIC_FQDN=$PUBLIC_FQDN \
+    -e PUBLIC_PORT=$PUBLIC_PORT \
+    -p $PUBLIC_PORT:1880 \
+    --cpus=$CPU_LIMIT \
+    node-red
+fi
--- object-detection-server/scripts/docker_run.sh
+++ object-detection-server/scripts/docker_run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-source ../../env.sh
-docker run --name od-server -it -d --restart=unless-stopped --cpus=$CPU_LIMIT -p 6001:6001 object-detection-server
+# set CPU limit for non-arm architecture (not supported by arm)
+if [[ "$(uname -m)" == "arm"* ]]; then
+    docker run --name od-server -it -d --restart=unless-stopped -p 6001:6001 object-detection-server
+else
+    source ../../env.sh
+    docker run --name od-server -it -d --restart=unless-stopped --cpus=$CPU_LIMIT -p 6001:6001 object-detection-server
+fi
+
